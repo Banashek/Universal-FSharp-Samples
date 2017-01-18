88a3543fa4fa687303e8c349eae59d7f2146c53d
Jonathan Banashek <jbanashek@gmail.com>
2017-01-18 13:09:55 -0800

    Live reloading suave working

--- UniversalLiveReloading/src/App.fs
+++ UniversalLiveReloading/src/App.fs
@@ -1,8 +1,40 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿// open FAKE
+open Suave
+open Suave.Files
+open Suave.Filters
+open Suave.Logging
+open Suave.Operators
+open Suave.RequestErrors
+open Suave.Successful
 open System
+open System.IO
+open System.Net
+
+let app =
+    choose [
+        path "/" >=> Files.browseFileHome "index.html"
+        path "/bundle.js" >=> Files.browseFileHome "bundle.js"
+        NOT_FOUND "404 - Not found."
+    ]
+
+
+let serverConfig port =
+  { defaultConfig with
+      logger = Targets.create LogLevel.Debug [||]
+      homeFolder = Some (Path.GetFullPath "./public")
+      bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port] }
+
+let rec startServer port =
+    try 
+        printfn "Starting with port %A" port
+        startWebServer (serverConfig port) app
+        0
+    with
+    | :? System.Net.Sockets.SocketException -> 
+        printfn "%A in use, incrementing port number." port
+        startServer (port + 1us)
+    | _ -> -1
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    startServer 8080us
